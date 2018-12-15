48a0ab05d9790cb2766f8ab46dc1fb7166cb7a97
Chrisjlan89 <chrisj17@gmail.com>
2018-12-15 09:02:54 -0500

    server  adjustments

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(routes)
 
 //app.use(express.static(path.join(__dirname, "client")))
 // ... other app.use middleware setups
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client", "public", "build")))
 
 
 
@@ -49,7 +49,7 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 // ...
 // Right before your app.listen(), add this:
  app.get("*", (req, res) => {  
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "public", "build", "index.html"));
  });
 
 app.listen(PORT, function(){
