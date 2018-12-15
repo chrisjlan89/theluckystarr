44b4935add7aed238e94126289943ced4b209d59
Chrisjlan89 <chrisj17@gmail.com>
2018-12-15 08:49:37 -0500

    2

--- server.js
+++ server.js
@@ -17,12 +17,15 @@ const PORT = process.env.PORT || 8081;
 
 
 
-// app.use(cors({
-// origin: 'http://localhost:3000',
-// credentials: true
-// }));
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
 // app.use(cors());
-app.options('*', cors());
+// app.options('*', cors());
 
 //app.use(express.static("./client/build"));
 
