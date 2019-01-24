aadaea12b931edc419ba3ce413728410f5bc11b5
Chrisjlan89 <chrisj17@gmail.com>
2019-01-24 16:55:41 -0500

    update to mobile responsiveness

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -25,12 +25,12 @@ render(){
     
     </div>
     <a href="#" className="brand-logo brand-text custom-brand-text"  >The Lucky Starr</a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><img className="mob-nav" src={process.env.PUBLIC_URL + "assets/img/hamburger-menu.png"}/></a>
-      <ul class="right hide-on-med-and-down">
-      <li className="text-color"><Link className={window.location.pathname === "/" ? "active" : ""}  to="/">   Home </Link></li>
-      <li className="text-color"><Link className={window.location.pathname === "/cabin" ? "active" : ""}  to="/cabin">   Cabin Rental </Link></li>
-      <li className="text-color"><Link className={window.location.pathname === "/venue" ? "active" : ""}  to="/venue">  The Venue </Link></li>
-      <li className="text-color"><Link className={window.location.pathname === "/form" ? "active" : ""} to="/form">   Contact Us </Link></li>
+      <a href="#" data-target="mobile-demo" className="sidenav-trigger sidenav-close "><img className="mob-nav" src={process.env.PUBLIC_URL + "assets/img/hamburger-menu.png"}/></a>
+      <ul className="right hide-on-med-and-down">
+      <li className="text-color " data-target="slide-out"><Link className={window.location.pathname === "/" ? "active" : ""}  to="/">   Home </Link></li>
+      <li className="text-color " ><Link className={window.location.pathname === "/cabin" ? "active" : ""}  to="/cabin">   Cabin Rental </Link></li>
+      <li className="text-color "><Link className={window.location.pathname === "/venue" ? "active" : ""}  to="/venue">  The Venue </Link></li>
+      <li className="text-color "><Link className={window.location.pathname === "/form" ? "active" : ""} to="/form">   Contact Us </Link></li>
       </ul>
       <a href ="https://www.facebook.com/luckystarr6086/">
       <img className ="social-container" src={process.env.PUBLIC_URL + "assets/img/Facebook_icon_20132.png"}/>
@@ -38,7 +38,7 @@ render(){
     </div>
   </nav>
 
-  <ul class="sidenav" id="mobile-demo">
+  <ul className="sidenav sidenav-close" id="mobile-demo">
   <li className="text-color"><Link className={window.location.pathname === "/" ? "active" : ""}  to="/">   Home </Link></li>
   <li className="text-color"><Link className={window.location.pathname === "/cabin" ? "active" : ""}  to="/cabin">   Cabin Rental </Link></li>
   <li className="text-color"><Link className={window.location.pathname === "/venue" ? "active" : ""}  to="/venue">  The Venue </Link></li>
