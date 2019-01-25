bdb17c9058e1533d6669a7f78638e261c95fc34d
Chrisjlan89 <chrisj17@gmail.com>
2019-01-25 17:20:19 -0500

    fixed some mobile styling, updated map and made it into own componet

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -35,10 +35,15 @@ let BASEURL = "https://photoslibrary.googleapis.com/v1/albums";
 
   sendEmail: function(emailData) {
     console.log(emailData, 'email data')
-    return axios.post('/api/send', emailData)
-  }
+    return axios.post('/api/send', emailData , {
+  }).then( 
+      (response) => { console.log(response) },
+      (error) => { console.log(error) }
+    );;
   }
 
+
+}
 
 
 
