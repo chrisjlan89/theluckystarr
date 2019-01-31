4f2eaebcf334357941b6cffeab8c2370136d605e
Chrisjlan89 <chrisj17@gmail.com>
2019-01-31 15:23:39 -0500

    moved nav to right for consistency across mobile

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -40,17 +40,18 @@ render(){
     
     </div>
     <a href="#" className="brand-logo brand-text custom-brand-text"  >The Lucky Starr</a>
+    <a  target="_blank" href ="https://www.facebook.com/luckystarr6086/">
+    <img className ="social-container" src={process.env.PUBLIC_URL + "assets/img/Facebook_icon_20132.png"} 
+    />
+    </a>
       <a href="#" data-target="mobile-demo"  className="sidenav-trigger sidenav-close"><img className="mob-nav" src={process.env.PUBLIC_URL + "assets/img/hamburger-menu.png"}/></a>
-      <ul className="right hide-on-med-and-down">
+      <ul className="left hide-on-med-and-down">
       <li className="text-color " data-target="slide-out"><Link className={window.location.pathname === "/" ? "active" : ""}  to="/">   Home </Link></li>
       <li className="text-color " ><Link className={window.location.pathname === "/cabin" ? "active" : ""}  to="/cabin">   Cabin Rental </Link></li>
       <li className="text-color "><Link className={window.location.pathname === "/venue" ? "active" : ""}  to="/venue">  The Venue </Link></li>
       <li className="text-color"><Link  className={window.location.pathname === "/form" ? "active" : ""} to="/form">   Contact Us </Link></li>
       </ul>
-      <a  target="_blank" href ="https://www.facebook.com/luckystarr6086/">
-      <img className ="social-container" src={process.env.PUBLIC_URL + "assets/img/Facebook_icon_20132.png"} 
-      />
-      </a>
+
     </div>
   </nav>
 
